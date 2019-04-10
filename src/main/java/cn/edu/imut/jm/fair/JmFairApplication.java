b8cfebb8f0ce71db7bda573fb29ba8506006ddc4
CuiYang16 <cuiyangmail@163.com>
2019-04-10 09:43:21 +0800

    first

---
+++ src/main/java/cn/edu/imut/jm/fair/JmFairApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.imut.jm.fair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmFairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmFairApplication.class, args);
+	}
+
+}
