b5f973d65f49a29f07138081459bdbdc72c421d2
CuiYang16 <cuiyangmail@163.com>
2019-05-05 19:04:03 +0800

    5-5

--- src/main/java/cn/edu/imut/jm/fair/domain/fair/valobj/FairUserVo.java
+++ src/main/java/cn/edu/imut/jm/fair/domain/fair/valobj/FairUserVo.java
@@ -1,19 +1,17 @@
 package cn.edu.imut.jm.fair.domain.fair.valobj;
 
-import java.util.List;
-
 import cn.edu.imut.jm.fair.domain.fair.entity.FairUser;
 
 public class FairUserVo extends FairUser {
 
-	private List<FairUserShowVo> users;
+	private FairUserShowVo joinUser;
 
-	public List<FairUserShowVo> getUsers() {
-		return users;
+	public FairUserShowVo getJoinUser() {
+		return joinUser;
 	}
 
-	public void setUsers(List<FairUserShowVo> users) {
-		this.users = users;
+	public void setJoinUser(FairUserShowVo joinUser) {
+		this.joinUser = joinUser;
 	}
 
 }
